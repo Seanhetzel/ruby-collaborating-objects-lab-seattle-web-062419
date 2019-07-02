bd0be0b3ee9efdc022c18ae94b4a0798fe724623
Sean Hetzel <seanhetzel1@gmail.com>
2019-07-02 00:18:35 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,37 @@
+class Artist
+
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def songs
+    @songs
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.find_or_create_by_name(name)
+    if @@all.include?(name)
+      @@all.select do |artist|
+        artist == name
+      end
+    else
+      Artist.save
+    end
+  end
+end
